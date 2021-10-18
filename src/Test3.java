e069d97a3db316f9653803a83751cca4caea9d3f
fazlida <92668221+fazlida@users.noreply.github.com>
2021-10-18 16:14:29 +0800

    Add if statement.

--- src/Test3.java
+++ src/Test3.java
@@ -4,6 +4,9 @@ public class Test3 {
 		// This is a simple output
 		   System.out.println("Welcome!");
 		   System.out.println("Welcome to Java!");
+		   
+		   if (1>0)
+			   System.out.println("Positive number!");
 	}
 
 }
