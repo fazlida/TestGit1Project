05b851cad762a0f673d5f3b904f5afabe952d152
fazlida <92668221+fazlida@users.noreply.github.com>
2021-10-18 16:03:48 +0800

    Commit Test3 to BranchX

---
+++ src/Test3.java
@@ -0,0 +1,9 @@
+
+public class Test3 {
+	public static void main (String[] args) {
+		// This is a simple output
+		   System.out.println("Welcome!");
+		   System.out.println("Welcome to Java!");
+	}
+
+}
